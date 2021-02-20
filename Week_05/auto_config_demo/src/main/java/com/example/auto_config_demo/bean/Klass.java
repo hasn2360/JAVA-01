9cbeb2148381a64d0013be9c94b639f5e580476f
hasn2360 <hasn2360@163.com>
2021-02-20 17:19:59 +0800

    删除无用代码

--- Week_05/auto_config_demo/src/main/java/com/example/auto_config_demo/bean/Klass.java
+++
@@ -1,9 +0,0 @@
-package com.example.auto_config_demo.bean;
-
-/**
- * @Auther: song.huai
- * @Date: 2021/2/18 23:46
- * @Description:
- */
-public class Klass {
-}
--- Week_05/auto_config_demo/src/main/java/com/example/auto_config_demo/config/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.auto_config_demo.config;
-
-/**
- * @Auther: song.huai
- * @Date: 2021/2/18 23:50
- * @Description:
- */
-public class WebConfig {
-}
